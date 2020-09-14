bf74ef9f98b698ee9a5fdbc441a6e9de34197b2f
Happy Guleria <dharmaniz.guleria@gmsil.com>
2020-09-14 12:24:46 +0530

    Initial Commit

---
+++ AppleLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleLogin
+//
+//  Created by Vivek Dharmani on 9/14/20.
+//  Copyright © 2020 Vivek Dharmani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
